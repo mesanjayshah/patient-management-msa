27df3a9aa9c85114b3624f100e5522814bc0a6e5
Sanjay Shah <messanjayshah31@gmail.com>
2025-03-14 18:29:04 +0545

    first commit

---
+++ patient-service/src/test/java/com/iesmac/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesmac.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
